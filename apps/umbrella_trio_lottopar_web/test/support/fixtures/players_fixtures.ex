ab1757b480eb2034483f053bc23be17b5fabf3b2
Elvis <eleram@protonmail.com>
2025-08-30 17:59:20 -0300

    feat: implementação da entidade Player (TASK_003)

---
+++ apps/umbrella_trio_lottopar_web/test/support/fixtures/players_fixtures.ex
@@ -0,0 +1,37 @@
+defmodule UmbrellaTrioLottoparWeb.PlayersFixtures do
+  @moduledoc """
+  This module defines test helpers for creating
+  entities via the `UmbrellaTrioLottoparWeb.Players` context.
+  """
+
+  @doc """
+  Generate a player.
+  """
+  def player_fixture(attrs \\ %{}) do
+    {:ok, player} =
+      attrs
+      |> Enum.into(%{
+        address: "some address",
+        birth_date: ~D[2025-08-29],
+        city: "some city",
+        country: "some country",
+        document: "some document",
+        email: "some email",
+        first_name: "some first_name",
+        gender: "some gender",
+        last_login: ~N[2025-08-29 20:54:00],
+        last_name: "some last_name",
+        op_id: "some op_id",
+        password: "some password",
+        phone: "some phone",
+        registration_date: ~D[2025-08-29],
+        state: "some state",
+        status: "some status",
+        username: "some username",
+        zip_code: "some zip_code"
+      })
+      |> UmbrellaTrioLottoparWeb.Players.create_player()
+
+    player
+  end
+end
